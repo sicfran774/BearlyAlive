c04cca5b5b3500fa42ef8a7784619e50cfefaa21
sicfran774 <sicfran.774@gmail.com>
2022-11-23 20:06:12 -0800

    scaffold for room generation

--- Bearly Alive/Assets/Scripts/LevelScripts/RoomManager.cs
+++ Bearly Alive/Assets/Scripts/LevelScripts/RoomManager.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using System.Threading;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 /*  Room generation must consist of a few things:
  *  1.  Where/what kind of enemies spawn
@@ -9,12 +11,22 @@ using UnityEngine;
  *  3.  A "room manager" that knows when/where a room CAN be generated
  *  - Use a "spawn manager" which reads a JSON file that contains location/type of each enemy in that room
  *  --> allows for enemies to spawn later than the beginning of the generation
+ *  
+ *  For room generation, I'm using one inspired by Binding of Issac and breadth-first search
+ *  It uses a grid of cells, then determines if each cell will have a room in it using a queue
+ *  It loops over a queue for all 4 directions (NSEW) and determines if the cell
+ *  1) has more than 1 neighbor
+ *  2) already has a room in that cell
+ *  if not, then make that cell a room and queue it
  */
 
 public class RoomManager : MonoBehaviour
 {
     [Header("Level Name")]
     public string levelName;
+
+    public int maxRooms;
+    public GameObject roomOne;
 
     [Space(20)]
 
@@ -27,10 +39,26 @@ public class RoomManager : MonoBehaviour
     [SerializeField] public bool playerEnteredRoom = false;
     private List<EnemyDataJson> enemyOrder;
 
+    private int roomCount = 0;
+    private int[] rooms;
+    private Queue<int> cellQueue;
+
+    System.Random rand = new System.Random();
+
     private void Start()
     {
         //SaveEnemyLocationsIntoFile();
         enemyOrder = EnemyPlaceScript.LoadEnemyData(Application.persistentDataPath + "/levelOne.json");
+
+        //Level generation
+        cellQueue = new Queue<int>();
+        rooms = new int[100];
+        for(int i = 0; i < 100; i++)
+        {
+            rooms[i] = 0;
+        }
+
+        GenerateLevel();
     }
 
     private void Update()
@@ -40,6 +68,11 @@ public class RoomManager : MonoBehaviour
             playerEnteredRoom = false;
             StartCoroutine(PlaceEnemies(enemyOrder));
         }
+    }
+
+    void GenerateLevel()
+    {
+        
     }
 
     IEnumerator PlaceEnemies(List<EnemyDataJson> enemyOrder)
