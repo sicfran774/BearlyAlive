55641124c0441df201450eee4a70e2902d4b598e
SkeltalFlamingo <masonnashcrete@outlook.com>
2022-11-30 22:02:55 -0800

    Revert "Merge branch 'main' of https://github.com/sicfran774/BearlyAlive"
    This reverts commit e297def32c38762a96eb12979da50871a6fd296a, reversing
    changes made to 0dbc8305dcb3a6fdc9cde0da9e4ccb028980d325.

--- Bearly Alive/Assets/Scripts/PlayerScripts/Slingshot.cs
+++ Bearly Alive/Assets/Scripts/PlayerScripts/Slingshot.cs
@@ -10,22 +10,17 @@ public class Slingshot : Technique
     public float slingSpeed = 300f;
     public float slingDuration = .7f;
 
-    public GameObject SlingshotBox; 
-
     // for applying movement to actor
     private Rigidbody2D actorBody;
 
     // for coordinating techniques
     private bool selfCooling;
-    public float selfCooldown = 1.1f;
+    private float selfCooldown = 1.1f;
 
 
     public override void Initialize (int damage = defaultDamage, float cooldown = defaultCooldown) {
         actorBody = gameObject.GetComponent<Rigidbody2D>();
         base.Initialize(defaultDamage, defaultCooldown);
-
-        // get SlingshotBox from gameObject
-        SlingshotBox = transform.GetChild(0).gameObject;
         
     }
 
@@ -34,11 +29,8 @@ public class Slingshot : Technique
     {
         if (!techsCooling && !selfCooling)
         {
-            // set coordination booleans
             techsCooling = true;
             selfCooling = true;
-
-            //perform the movement and enable/diable hitbox
             StartCoroutine(HandleSlingShot(slingDuration));
         }
 
@@ -61,20 +53,14 @@ public class Slingshot : Technique
     {
         float t = 0.0f;
 
-        // enable damaging/protecting hitbox holder
-        SlingshotBox.SetActive(true);
-
         // lock movement and direction
         moveLock = true;
         cursorLock = true;
-
-        // pause
         while(t < cooldown - duration) {
             t+= Time.deltaTime;
             
             yield return null;
         }
-        // sling
         while (t < cooldown)
         {
             move();
@@ -82,15 +68,11 @@ public class Slingshot : Technique
             yield return null;
         }
 
-        // disable damaging/protecting hitbox holder
-        SlingshotBox.SetActive(false);
-
         // unlock movement and direction
         moveLock = false;
         cursorLock = false;
         techsCooling = false;
 
-        // cool self
         print(t);
         while (t < selfCooldown)
         {
