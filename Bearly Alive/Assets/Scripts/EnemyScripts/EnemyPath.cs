4f07d4d0e86262a778b32595365d2ce45431e81d
imhess <79474657+imhess@users.noreply.github.com>
2022-11-30 18:05:48 -0800

    Pathfinding, Spicy/Sour, and Enemy QOL
    A lot of stuff is new here. Enemies now have a built-in pathfinder with some custom changeable variables. Almost everything is set to make our unique enemy creation process as smooth as possible.

---
+++ Bearly Alive/Assets/Scripts/EnemyScripts/EnemyPath.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPath : MonoBehaviour
+{
+    public AIPath aIPath;
+
+    Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceVelocity();
+    }
+
+    void faceVelocity() {
+        direction = aIPath.desiredVelocity;
+
+        transform.right = direction;
+    }
+}
