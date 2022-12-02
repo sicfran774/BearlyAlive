67dd722a362e62a44de3ee54f95955aefe7630d6
Say my name <ceiaram@cpp.edu>
2022-12-01 20:41:17 -0800

    optimized setUpgrade

--- Bearly Alive/Assets/Scripts/UIScripts/UpgradeUIManager2.cs
+++ Bearly Alive/Assets/Scripts/UIScripts/UpgradeUIManager2.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class UpgradeUIManager2 : MonoBehaviour
 {
-
     //upgrade menu object
     public GameObject upgradeMenu;
     public bool[] isUpgraded = new bool[2];
@@ -22,16 +21,27 @@ public class UpgradeUIManager2 : MonoBehaviour
 
     //Apply upgrade based on what technique player chooses 
     public void setUpgradeinSlot1()
+    {   
+        //First Slot is selected to be upgraded 
+        isUpgraded[0] = true;
+        setUpgradeinSlot(1);
+    }
+
+    public void setUpgradeinSlot2()
+    {
+        //Second Slot is selected to be upgraded 
+        isUpgraded[1] = true;
+        setUpgradeinSlot(2);
+    }
+
+
+    // to be called by setUpgradeinSlot2() and ...1()
+    public void setUpgradeinSlot(int slot)
     {
         string selectedUpgrade = PlayerController.instance.pickedUpgrade.name;
-        //ToDo:
-        isUpgraded[0] = true;
+        PlayerController.instance.setUpgrade(slot, selectedUpgrade);
 
-        print("Is First upgraded: " + isUpgraded[0]);
-
-        print(selectedUpgrade + " upgrade applied to slot 1");
-
-        PlayerController.instance.setUpgrade(1, selectedUpgrade);
+        print(selectedUpgrade + " upgrade applied to slot " + slot);
 
         //Display upgrade image in UI first slot 
         if (isUpgraded[0] == true)
@@ -47,7 +57,7 @@ public class UpgradeUIManager2 : MonoBehaviour
         if (isUpgraded[1] == true)
         {
             Sprite getSprite = SpriteUI.instance.sprite;
-         
+
             GameManager.instance.upgradeImageSecondSlot = GetComponent<Image>();
             GameManager.instance.upgradeImageSecondSlot.sprite = getSprite;
         }
@@ -61,30 +71,4 @@ public class UpgradeUIManager2 : MonoBehaviour
         //Destroy upgrade
         Destroy(PlayerController.instance.pickedUpgrade);
     }
-
-    public void setUpgradeinSlot2()
-    {
-        string selectedUpgrade = PlayerController.instance.pickedUpgrade.name;
-
-        //ToDo:
-        isUpgraded[1] = true;
- 
-        print("Is Second upgraded: " + isUpgraded[1]);
-
-        print(selectedUpgrade + " upgrade applied to slot 2");
-
-        PlayerController.instance.setUpgrade(2, selectedUpgrade);
-
-        //Disable upgrade menu when player selects technique to upgrade 
-        upgradeMenu.SetActive(!upgradeMenu.activeSelf);
-
-        //If upgrade menu is active, pass argument in delegate 
-        GameManager.instance.onToggleUpgradeMenu.Invoke(upgradeMenu.activeSelf);
-
-        //Destroy upgrade
-        Destroy(PlayerController.instance.pickedUpgrade);
-    }
-
-
-
 }
