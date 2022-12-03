d8db1861df48dd6e98838e7b3783e5f950f3e2a0
Maya Tene <maya@Piglets-Tail.local>
2022-12-03 13:19:52 -0800

    death animation script added

---
+++ Bearly Alive/Assets/Scripts/DeathAnimation.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathAnimation : MonoBehaviour
+{
+   public Sprite dying;
+   public Sprite dead;
+
+   public SpriteRenderer spriteRenderer;
+
+   private void Reset()
+   {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+   }
+
+   private void OnEnable() 
+   {
+        UpdateSprite();
+        DisablePhysics();
+        StartCoroutine(Animate());
+        
+   }
+
+   private void UpdateSprite()
+   {
+          spriteRenderer.enabled = true;
+          spriteRenderer.sortingOrder = 10;
+          if (dying != null) {
+              spriteRenderer.sprite = dying; 
+          }
+          
+   }
+
+   private void DisablePhysics()
+   {
+          Collider2D[] colliders = GetComponents<Collider2D>();
+          foreach (Collider2D collider in colliders) {
+               collider.enabled = false;
+          }
+
+          GetComponent<Rigidbody2D>().isKinematic = true;
+   }
+
+   private IEnumerator Animate()
+   {
+          float elapsed = 0f;
+          float duration = 1f;
+
+          while (elapsed < duration) {
+               spriteRenderer.sprite = dying;
+               elapsed += Time.deltaTime;
+               yield return null;
+          }
+
+          spriteRenderer.sprite = dead;
+   }
+
+
+}
