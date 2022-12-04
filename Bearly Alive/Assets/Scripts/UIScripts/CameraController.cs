80ffbd0687724e6355512e00eb8ac48b11747636
sicfran774 <sicfran.774@gmail.com>
2022-12-04 02:24:17 -0800

    Camera movement temp snapped to player

--- Bearly Alive/Assets/Scripts/UIScripts/CameraController.cs
+++ Bearly Alive/Assets/Scripts/UIScripts/CameraController.cs
@@ -15,6 +15,10 @@ public class CameraController : MonoBehaviour
     // Follows Player Objects Movement
     void cameraMovement()
     {
-        transform.position = new Vector3(roomManager.playerCurrentRoom.transform.position.x, roomManager.playerCurrentRoom.transform.position.y, transform.position.z);
+        //This code below will snap camera to each room
+        //transform.position = new Vector3(roomManager.playerCurrentRoom.transform.position.x, roomManager.playerCurrentRoom.transform.position.y, transform.position.z);
+
+        //Old code
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
     }
 }
