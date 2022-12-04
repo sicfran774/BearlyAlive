934f9e5723a9f0f44aa4a1d90b14fd1fa9ed34b9
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2022-12-03 21:51:36 -0800

    Added Separate Variables for PickedUp Technique and Modifed 'T" to open WeaponSwap Menu

--- Bearly Alive/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Bearly Alive/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -75,13 +75,20 @@ public class PlayerController : MonoBehaviour
 
     //Text object when upgrade is collided 
     [SerializeField]
-    private Text pressFLabel; 
+    private Text pressFLabel;
+
+    [SerializeField]
+    private Text pressTLabel;
 
     //Upgrade UI attributes
-    public bool canpickup;
+    public bool canpickupUpgrade;
+
+    public bool canpickupTechnique;
 
     //public string pickedUpgrade;
     public GameObject pickedUpgrade;
+
+    public GameObject pickedTechnique;
 
 
     // Start is called before the first frame update
@@ -100,6 +107,8 @@ public class PlayerController : MonoBehaviour
         //Method called when delegate is invoked 
         UpgradeUI.instance.onToggleUpgradeMenu += OnUpgradeMenuToggle;
 
+        TechniqueUIManager.instance.onToggleWeaponMenu += OnWeaponMenuToggle;
+
         //Refresh HUD at the start of the game
         hudManager.refresh();
 
@@ -113,8 +122,12 @@ public class PlayerController : MonoBehaviour
 
         //Upgrade pick up attributes 
         pressFLabel.enabled = false;
-        canpickup = false;
+        canpickupUpgrade = false;
         pickedUpgrade = null;
+
+        canpickupTechnique = false;
+        pickedTechnique = null;
+        pressTLabel.enabled = false;
     }
 
     private void Awake()
@@ -148,6 +161,19 @@ public class PlayerController : MonoBehaviour
             GetComponent<Technique>().enabled = !active;
         }
     }
+
+
+    void OnWeaponMenuToggle(bool active)
+    {
+        //Disable player movement and shooting
+        GetComponent<PlayerController>().enabled = !active;
+
+        if (GetComponent<Technique>() != null)
+        {
+            GetComponent<Technique>().enabled = !active;
+        }
+    }
+
 
     private void FixedUpdate()
     {
@@ -238,8 +264,15 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Technique")
         {
-            print("ENTERED Technique"); 
-            print(collision.gameObject.name);
+            //Display label on UI
+            pressTLabel.enabled = true;
+
+            //Player can now pick up upgrade
+            canpickupTechnique = true;
+
+            //Assign picked upgrade for upgrade menu UI
+            pickedTechnique = collision.gameObject;
+
         }
 
         if (collision.gameObject.tag == "Upgrade")
@@ -250,12 +283,11 @@ public class PlayerController : MonoBehaviour
             pressFLabel.enabled = true;
 
             //Player can now pick up upgrade
-            canpickup = true;
+            canpickupUpgrade = true;
 
             //Assign picked upgrade for upgrade menu UI
             pickedUpgrade = collision.gameObject;
 
-            print(collision.gameObject.name);
         }
 
         if (collision.gameObject.tag == "EnemyBullet")
@@ -284,9 +316,19 @@ public class PlayerController : MonoBehaviour
 
             pickedUpgrade = null;
 
-            canpickup = false;
+            canpickupUpgrade = false;
 
         }
+        else if(collision.gameObject.tag == "Technique")
+        {
+            pressTLabel.enabled = false;
+
+            pickedTechnique = null;
+
+            canpickupTechnique = false;
+        }
+
+
     }
 
     private void OnTriggerStay2D(Collider2D other)
