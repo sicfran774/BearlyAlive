1160fe3c7022098f5631c694fc4549dc3e16533d
Maya Tene <maya@Piglets-Tail.lan>
2022-12-05 23:28:27 -0800

    C# documentation for prof

--- Bearly Alive/Assets/Scripts/EnemyScripts/BossScripts/Extensions.cs
+++ Bearly Alive/Assets/Scripts/EnemyScripts/BossScripts/Extensions.cs
@@ -1,3 +1,15 @@
+/*************************************************************** 
+*file: AnimatedSprite.cs 
+*author: M. Tene 
+*class: CS 4700 – Game Development 
+*assignment: program 4 
+*date last modified: 12/4/2022 
+* 
+*purpose: this program handles sprite collision logic for the boss. 
+* 
+****************************************************************/ 
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
